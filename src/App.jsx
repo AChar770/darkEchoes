8940308794305159509d8f561af63f265b6f69a0
AChar770 <amandatcharles@yahoo.com>
2026-02-06 14:26:22 -0500

    numbers 1 and 2 of readMe initiated

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
-export default function App() {
-  // TODO
-}
+import { useState } from "react";
+import { episodeList } from "./data";
+
+// export default function App() {
+//   const [episode, setEpisodes] = useState(data); // data array stored in state
+const listItems = episodeList.map(episode => <li>{#title}</li>/li>)
+return <ul>{#title}</ul>
