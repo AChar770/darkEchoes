f9ee4bcc9ae9812ebd931691a9d9e9b4bdc8ca51
AChar770 <amandatcharles@yahoo.com>
2026-02-07 16:57:02 -0500

    episode list added

--- src/App.jsx
+++ src/App.jsx
@@ -3,5 +3,25 @@ import { episodeList } from "./data";
 
 // export default function App() {
 //   const [episode, setEpisodes] = useState(data); // data array stored in state
-const listItems = episodeList.map(episode => <li>{#title}</li>/li>)
+const listItems = episodeList.map(episode => 
 return <ul>{#title}</ul>
+
+
+function EpisodeList() {
+    return (
+      <section className="episodes">
+        <h2>List of Episodes</h2>
+        <ol>
+          {episodes.map((episode) => (
+            <li
+              key={episode.id}
+              onClick={() => setSelectedEpisode(episode)}
+              className={selectedEpisode?.id === episode.id ? "selected" : ""}
+            >
+              {episode.title}
+            </li>
+          ))}
+        </ol>
+      </section>
+    );
+  }
