195ff540f3121fd6630fb9d22eaae1e4bf188128
AChar770 <amandatcharles@yahoo.com>
2026-02-10 17:53:11 -0500

    export default function placed

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
 import { useState } from "react";
 import { episodeList } from "./data";
 
-// export default function App() {
-//   const [episode, setEpisodes] = useState(data); // data array stored in state
-const listItems = episodeList.map(episode => 
-return <ul>{#title}</ul>
+export default function App() {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
 
+
+const listItems = episodeList.map(episode =>
+return <ul>{#epTitle}</ul>
 
 function EpisodeList() {
-    return (
-      <section className="episodes">
+  return (
+    <section className="episodes">
         <h2>List of Episodes</h2>
         <ol>
           {episodes.map((episode) => (
-            <li
+            <li>
               key={episode.id}
               onClick={() => setSelectedEpisode(episode)}
-              className={selectedEpisode?.id === episode.id ? "selected" : ""}
-            >
-              {episode.title}
+              className={selectedEpisode?.id === episode.id ? "selected" : ""}{episode.epTitle}
             </li>
           ))}
         </ol>
       </section>
     );
   }
+
+  return (
+    <>
+      <header>
+        <h1>Dark Echoes</h1>
+      </header>
+      <main>
+        <EpisodeList />
+        <EpisodeDetails />
+      </main>
+    </>
+  );
+}
